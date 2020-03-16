a1e72fb25b02ee1b86382a5234e91f8fd22c6f20
EEGod <9635792+eegod@users.noreply.github.com>
2020-03-16 09:54:37 +0800

    [bash] cd w/ ls

---
+++ shell/.bashrc.d/cmd.sh
@@ -0,0 +1,12 @@
+# cd with ls
+function cl() {
+    DIR="$*";
+        # if no DIR given, go home
+    if [ $# -lt 1 ]; then
+        DIR=$HOME;
+    fi;
+    builtin cd "${DIR}" && \
+    # use your preferred ls command
+        ls -F --color=auto
+}
+
