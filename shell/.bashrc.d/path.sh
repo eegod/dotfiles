7f06ee64ddd58a3972f10d64c58e531aaebc8b98
EEGod <9635792+eegod@users.noreply.github.com>
2020-10-11 08:39:20 +0800

    [homebrew] update path

--- shell/.bashrc.d/path.sh
+++ shell/.bashrc.d/path.sh
@@ -1,11 +1,18 @@
 # GNU exec
-export PATH="/usr/local/opt/sqlite/bin:$PATH"
+# export PATH="/usr/local/opt/sqlite/bin:$PATH"
 export PATH="/usr/local/opt/gettext/bin:$PATH"
 export PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
 export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
+
+# export PATH="/Users/whan/Library/Python/3.7/bin:$PATH"
+
 export MANPATH="/usr/local/opt/coreutils/libexec/gnuman:$MANPATH"
+
+# sqlite
+export PATH="/usr/local/opt/sqlite/bin:$PATH"
 # export LDFLAGS="-L/usr/local/opt/sqlite/lib"
 # export CPPFLAGS="-I/usr/local/opt/sqlite/include"
+# export PKG_CONFIG_PATH="/usr/local/opt/sqlite/lib/pkgconfig"
 
 # export LDFLAGS="-L/usr/local/opt/gettext/lib"
 # export CPPFLAGS="-I/usr/local/opt/gettext/include"
